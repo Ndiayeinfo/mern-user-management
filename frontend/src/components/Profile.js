e7fe89864a5d0318a314a78f1c408fd0c55838f1
Ndiayeinfo <youssoundiaye.yn.yn@gmail.com>
2025-04-25 19:35:47 +0000

    Ajout .env.development et .env.production + fallback API_URL

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -9,7 +9,7 @@ const Profile = () => {
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const res = await axios.get('http://localhost:5000/api/auth/me', {
+        const res = await axios.get('/auth/me', {
           headers: {
             Authorization: `Bearer ${token}`,
           },
