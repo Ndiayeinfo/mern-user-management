1325b05a8ccabc4ae54963667a07213922342240
Ndiayeinfo <youssoundiaye.yn.yn@gmail.com>
2025-04-26 18:08:58 +0000

    Préparation déploiement prod avec proxy Nginx + CORS

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,9 @@ app.get('/', (req, res) => {
   res.send('API en ligne');
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Serveur lancé sur le port ${PORT}`));
-
 const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Serveur lancé sur le port ${PORT}`));
 
